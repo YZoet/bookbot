46feb6518db689a4d3ced0d0226b8bb0df35b116
xyz <Masteremail>
2024-11-24 15:19:49 +0100

    added count

--- main.py
+++ main.py
@@ -2,4 +2,17 @@ with open('books/frankenstein.txt','r') as file:
         Books = file.read();
         #print(Books);
         words = Books.split()
-print(len(words));
+        print(len(words));
+        CharCount = Books;
+        LowChar = CharCount.lower(); 
+        Letters = count_letters(LowChar)
+        
+        
+def count_letters(s):
+        letter_counts = {}
+        for char in s:
+                if char in letter_counts:
+                        letter_counts[char] += 1
+                else:
+                        letter_counts[char] = 1
+        return letter_counts
