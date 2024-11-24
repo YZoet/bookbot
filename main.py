0f34a13f8d05908935ede0150d94d7c79a9872ff
xyz <Masteremail>
2024-11-24 15:01:08 +0100

    Added word count again

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ with open('books/frankenstein.txt','r') as file:
         Books = file.read();
         #print(Books);
         words = Books.split
-count = len(words);
-print(count);
+print(len(words));
